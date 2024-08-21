423879aea76bf43ef81086e9e78842afe60a1288
Cristian <cristianmayco@gmail.com>
2024-08-20 21:05:54 -0300

    Base project

---
+++ src/test/kotlin/com/souls/boot/SoulsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.souls.boot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoulsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
